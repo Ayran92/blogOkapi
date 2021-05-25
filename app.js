b5a2a66bcfca70544375ca64cd86daa540642356
Ayran <ayranbrito.c@gmail.com>
2021-05-24 21:25:34 -0300

    Implementacao da criacao do posts no banco de dados e o print dos posts na home

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const rotaEstanteDeLivros = require("./routes/estanteDeLivros");
 const roteadorMaisLidos = require("./routes/maisLidos");
 const roteadorContato = require("./routes/contato");
 const roteadorLogin = require("./routes/login");
 const roteadorPainelPosts = require("./routes/painel-posts");
+const Posts = require("./models/Post");
 const app  = express();
 
+mongoose.connect("mongodb://localhost/okapiBlog", {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+});
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: false}));
 app.use(express.static("public"));
 
 
-app.get("/", (req, res) => {
-    res.render("./pages/home");
+app.get("/", async (req, res) => {
+    const posts = await Posts.find().sort({dataCriacao: "desc"})
+    res.render("./pages/home", { posts: posts });
 });
 
 app.use("/estante-de-livros", rotaEstanteDeLivros);
